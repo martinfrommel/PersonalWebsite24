1c9647da52611eb301746e4676daa181aae8d916
martinfrommel <martin.frommel@gmail.com>
2024-02-22 10:01:51 +0100

    Update project configuration and styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,22 +1,23 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["EB Garamond Variable", ...defaultTheme.fontFamily.sans],
-        serif: ["Rockwell", ...defaultTheme.fontFamily.serif],
-        mono: ["VT323", ...defaultTheme.fontFamily.mono],
-      },
-      colors: {
-        dark: "#303037",
-        light: "#e2e9e0",
-      },
-      screens: {
-        xs: "370px",
-        ...defaultTheme.screens,
-      },
-    },
-  },
-  plugins: [require("daisyui")],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['EB Garamond Variable', ...defaultTheme.fontFamily.sans],
+				serif: ['Rockwell', ...defaultTheme.fontFamily.serif],
+				mono: ['VT323', ...defaultTheme.fontFamily.mono]
+			},
+			colors: {
+				dark: '#303037',
+				light: '#e2e9e0'
+			},
+			screens: {
+				xs: '370px',
+				...defaultTheme.screens
+			}
+		}
+	}
+}

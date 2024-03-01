0e7b5f25b23d13610a321c55de77666eac563ea5
martinfrommel <martin.frommel@gmail.com>
2024-03-01 11:51:38 +0100

    Update content collection schema and rename [slug].astro to [...slug].astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,1 +1,36 @@
-import { defineCollection, z } from 'astro:content'
+import { Schema } from "astro/zod";
+import { defineCollection, z } from "astro:content";
+
+const languages = ["en", "cz"] as const;
+
+const technologies = [
+  "javascript",
+  "typescript",
+  "wordpress",
+  "react",
+  "node",
+  "php",
+  "html",
+  "css",
+  "express",
+  "tailwind",
+  "graphQL",
+  "mongoDB",
+  "postgres",
+  "mysql",
+  "firebase",
+  "aws",
+  "docker",
+] as const;
+
+const projectsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    datePublished: z.date(),
+    language: z.enum(languages).optional(),
+    technologies: z.array(z.enum(technologies)),
+    description: z.string(),
+    image: z.string().optional(),
+  }),
+});

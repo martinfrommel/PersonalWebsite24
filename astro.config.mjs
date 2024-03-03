31e8a37aac2fe35f10869a7c781df37ae414c84a
martinfrommel <martin.frommel@gmail.com>
2024-03-03 19:43:01 +0100

    Add astro/sitemap integration for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://martin.frommel.cz",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    icon(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), icon(), sitemap()]
 });

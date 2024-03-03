42718bf1d1cdf9e3dfbaba3274970543f58083e5
martinfrommel <martin.frommel@gmail.com>
2024-03-03 19:38:43 +0100

    Add site name to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
+import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), icon()]
+  site: "https://martin.frommel.cz",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+  ],
 });
